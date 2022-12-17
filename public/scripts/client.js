f084e7b347ac0d7dbe71d4bbb36692e17348dabc
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-12-17 12:53:53 -0500

    linted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,23 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-
-
-  //Renders the tweet data by appending tweet html to #tweet-container form element"
-// Fake data taken from initial-tweets.json
+//function to ensure that the tweets are loaded as soon as the page is ready.
 $(document).ready(function() {
 
+  //Hide error messages temporarily
   $("#error-message-empty").hide();
   $("#error-message-tooLong").hide();
 
-  //escape function for safe user input
-const escape = function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  //takes a string as input and returns an escaped version of the string with any potentially dangerous characters converted to HTML entities. This is used to ensure that user input is properly sanitized before being displayed on the page.
+  const escape = function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
+  //takes a tweet object as input and returns an HTML element representing the tweet. The HTML element is constructed using a template string and the properties of the tweet object, such as the user's name and the tweet text.
   const createTweetElement = function(tweetData) {
     let $tweet = $(`<article class="tweet">
           <header class="tweet-header">
@@ -44,32 +42,37 @@ const escape = function(str) {
             </div>
           </footer>
         </article>`);
-      return $tweet;
+    return $tweet;
   };
 
-  const renderTweets = function(tweets) {
-    $('#tweets-container').empty()
-    for (let tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
-    }
-  };
+//takes an array of tweet objects as input and renders them to the page by appending each tweet's HTML element to a container element on the page.
+const renderTweets = tweets => {
+  const $tweetsContainer = $('#tweets-container');
+  $tweetsContainer.empty();
 
+  tweets.forEach(tweet => {
+    const $tweet = createTweetElement(tweet);
+    $tweetsContainer.append($tweet);
+  });
+};
+
+
+  //makes an AJAX GET request to the server to retrieve a list of tweets, and then passes the retrieved data to the renderTweets function to display the tweets on the page.
   const loadTweets = function() {
     $.ajax({
       url: "/tweets",
       method: "GET",
     })
-    .then(data => {
-      renderTweets(data)
-    })
+      .then(data => {
+        renderTweets(data);
+      });
   };
-  
+
+  //Adds new tweet when clicking submit
   $('form').submit(function(event) {
     event.preventDefault();
     const maxChar = 140;
     const inputLength = $(this).find("#tweet-text").val().length;
-    const formData = $(this).serialize();
     $("#error-message-empty").slideUp("slow");
     $("#error-message-tooLong").slideUp("slow");
     if (!inputLength) {
@@ -84,7 +87,7 @@ const escape = function(str) {
         method: "POST",
         data: $('form').serialize()
       })
-      .then(loadTweets)
+        .then(loadTweets);
       $(this).children().find('textarea').val('');
       $('.counter').text(140);
     }
